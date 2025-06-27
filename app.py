d9690ed7256499c50bad5fe40fe0745115d34eb8
Yane.n <100201975+BULLSSS@users.noreply.github.com>
2025-06-27 14:37:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify(message="Hello from Flask API!")
+    return jsonify(message="Hello from my UPDATED Flask API!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True) # Ajoutez debug=True
